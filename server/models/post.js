d7548ce4063a45f8425f0e70ce5782820a0bc62b
Andrew <andrewrandolph98@gmail.com>
2023-07-12 13:42:54 -0500

    Added connection.js and added models layout

---
+++ server/models/post.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../Utils/dateFormat");
+
+const postSchema = new Schema({
+  postAuthor: {
+    type: String,
+    // required: true,
+    trim: true,
+  },
+  image: {
+    type: String,
+  },
+  message: {
+    type: String,
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
+  tags: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  comments: [
+    {
+      commentAuthor: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 256,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
+});
+
+const post = model("post", postSchema);
+
+module.exports = post;
